51ae62c7eb0a2c06d2b6526210bf7b33bba3daea
Juan <juan.gige95@gmail.com>
2024-04-17 18:21:22 -0300

    errores

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+const firebaseConfig = import.meta.env.VITE_FIREBASE_CONFIG;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
