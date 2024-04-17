8dccafc255bf2240f0827aa66a21ae3374833f32
Juan <juan.gige95@gmail.com>
2024-04-17 18:33:08 -0300

    errores v3

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -4,7 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+const firebaseConfig = {
+  apiKey: "AIzaSyDvhDyuFWPIOiIWvDgr_XhmChLXPGzJ-bM",
+  authDomain: "jtech-2e9d3.firebaseapp.com",
+  projectId: "jtech-2e9d3",
+  storageBucket: "jtech-2e9d3.appspot.com",
+  messagingSenderId: "9654694063",
+  appId: "1:9654694063:web:8fbea551bbba6ac814e45a"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
